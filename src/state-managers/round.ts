db732caf9d28b1df2d9a0746a15b472ae483259a
Tynahan <nusilite@gmail.com>
2026-03-08 05:36:31 +0900

    feat: add simple review logic

--- src/state-managers/round.ts
+++ src/state-managers/round.ts
@@ -3,13 +3,17 @@ import screen from "./screen.js"
 
 // TODO: 단순화하자. sublevel, shuffle 없애고 level 올라갈 때마다 endIndex 올리자
 // TODO: 통계를 이용하는 건 round 이니까 round에서 통계 관련 속성을 가지고 있는 게 자연스럽겠다
+type StartRoundProps = {
+    startIndex: number
+    endIndexDiff: number
+}
 type Round = {
     endIndex: number // NOTE: starts from 1
     startIndex: number // TODO: 지금은 0으로 고정하고 subLevel은 나중에 구현하자
     targetText: string // NOTE: 이걸 친다
     targetScore: number // NOTE: 이것의 절반보다 낮으면 sub 시작
 
-    startRound: (endIndexDiff: number) => void
+    startRound: ({ startIndex, endIndexDiff }: StartRoundProps) => void
     handleRoundResult: (score: number) => void
 }
 const round: Round = {
@@ -18,9 +22,11 @@ const round: Round = {
     targetText: wordBank[0] ?? "the",
     targetScore: 80,
 
-    startRound: (endIndexDiff: number) => {
+    startRound: ({ startIndex, endIndexDiff }: StartRoundProps) => {
         screen.startTime = Date.now()
         round.endIndex += endIndexDiff
+
+        round.startIndex = startIndex < round.endIndex ? startIndex : round.endIndex - 1
         round.targetText = wordBank
             .slice(round.startIndex, round.endIndex)
             .sort(() => -1)
@@ -30,12 +36,24 @@ const round: Round = {
     },
     handleRoundResult: (score: number) => {
         if (score > round.targetScore) {
-            // NOTE: 다음 라운드 시작
-            round.startRound(1)
+            // NOTE: 다 잘했으면 다음으로
+            if (round.startIndex <= 0) {
+                round.startRound({ startIndex: 0, endIndexDiff: 1 })
+                return
+            }
+            // NOTE: 복습 잘했으면 복습 단어 추가
+            round.startRound({ startIndex: round.startIndex - 1, endIndexDiff: 0 })
             return
         }
-        // NOTE: 지금 라운드 반복
-        round.startRound(0)
+
+        // NOTE: 아주 못하면 개수 하나 줄여 재시도
+        if (score < round.targetScore / 2) {
+            round.startRound({ startIndex: round.startIndex + 1, endIndexDiff: 0 })
+            return
+        }
+
+        // NOTE: 적당히 못하면 반복
+        round.startRound({ startIndex: round.startIndex, endIndexDiff: 0 })
     },
 }
 
